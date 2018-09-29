b95d98cbf701280431c8ed1a2f17f65b0ef7453a
gomesuit <9404411+gomesuit@users.noreply.github.com>
2018-09-29 18:22:39 +0900

    save

---
+++ v6.0/install-varnish.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+
+# for build vaninsh
+yum install -y \
+    make \
+    autoconf \
+    automake \
+    jemalloc-devel \
+    libedit-devel \
+    libtool \
+    ncurses-devel \
+    pcre-devel \
+    pkgconfig \
+    python-docutils \
+    python-sphinx
+
+cd /tmp
+wget https://github.com/varnishcache/varnish-cache/archive/varnish-6.0.1.tar.gz
+tar zxfv varnish-6.0.1.tar.gz
+cd varnish-cache-varnish-6.0.1/
+
+sh autogen.sh
+sh configure
+make
+make install
+
+
+groupadd -g 992 varnish
+# -Mオプション(--no-create-homeオプション)：ホームディレクトリを作成しない
+useradd -c 'Varnish Cache' -g 992 -u 995 -M -s /sbin/nologin -d /var/lib/varnish varnish
+
+cp /vagrant/settings/vaninsh.service /usr/lib/systemd/system/varnish.service
